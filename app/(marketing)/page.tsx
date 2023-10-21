f24bcf61633a07f6da25a8cee554fe6ef8aa9587
Mohamad Salimi <contact@salimi.my>
2023-10-22 00:16:24 +0800

    Add heading & heroes section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
-import Heading from '@/app/(marketing)/_components/heading';
+import Heading from '@/components/marketing/heading';
+import Heroes from '@/components/marketing/heroes';
 
 const MarketingPage = () => {
   return (
     <div className='min-h-full flex flex-col'>
       <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
