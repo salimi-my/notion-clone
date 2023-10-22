4c829abbb0291d1c40bef3cf2c702efeb6eab413
Mohamad Salimi <contact@salimi.my>
2023-10-22 22:32:28 +0800

    Update issuer domain

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,7 +2,7 @@
 export default {
   providers: [
     {
-      domain: 'https://needed-blowfish-65.clerk.accounts.dev',
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: 'convex'
     }
   ]
