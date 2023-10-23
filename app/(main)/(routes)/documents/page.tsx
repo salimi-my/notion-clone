8ad68b725ec96aaad271c8ae5758affe79f21b2f
Mohamad Salimi <contact@salimi.my>
2023-10-23 23:01:50 +0800

    Create convex schema & api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,13 +1,27 @@
 'use client';
 
 import Image from 'next/image';
+import { toast } from 'sonner';
 import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
 import { useUser } from '@clerk/clerk-react';
 
+import { api } from '@/convex/_generated/api';
 import { Button } from '@/components/ui/button';
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: 'Untitled' });
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note.'
+    });
+  };
 
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
@@ -28,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className='text-lg font-medium'>
         Welcome to {user?.firstName}&apos;s Motion
       </h2>
-      <Button onClick={() => {}}>
+      <Button onClick={onCreate}>
         <PlusCircle className='h-4 w-4 mr-2' />
         Create a note
       </Button>
