aca00d09694d317fbc0c9d6187063d93dd53b13d
Mohamad Salimi <contact@salimi.my>
2023-10-24 23:30:41 +0800

    Add sidebar items

--- components/main/navigation.tsx
+++ components/main/navigation.tsx
@@ -1,14 +1,25 @@
+import { toast } from 'sonner';
+import { useMutation } from 'convex/react';
 import { useMediaQuery } from 'usehooks-ts';
 import { usePathname } from 'next/navigation';
-import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { useRef, ElementRef, useState, useEffect } from 'react';
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
+  Settings
+} from 'lucide-react';
 
 import { cn } from '@/lib/utils';
+import Item from '@/components/main/item';
+import { api } from '@/convex/_generated/api';
 import UserItem from '@/components/main/user-item';
 
 const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<'aside'>>(null);
@@ -90,6 +101,16 @@ const Navigation = () => {
     }
   };
 
+  const handleCreate = () => {
+    const promise = create({ title: 'Untitled' });
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'New note created!',
+      error: 'Failed to create a new note.'
+    });
+  };
+
   return (
     <>
       <aside
@@ -112,6 +133,9 @@ const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label='Search' icon={Search} isSearch onClick={() => {}} />
+          <Item label='Settings' icon={Settings} onClick={() => {}} />
+          <Item onClick={handleCreate} label='New page' icon={PlusCircle} />
         </div>
         <div className='mt-4'>
           <p>Documents</p>
