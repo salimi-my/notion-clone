bf18f715235ec7740103171b097961a80935ac9f
Mohamad Salimi <contact@salimi.my>
2023-10-25 22:20:39 +0800

    Add getSearch api endpoint

--- convex/documents.ts
+++ convex/documents.ts
@@ -207,3 +207,24 @@ export const remove = mutation({
     return document;
   }
 });
+
+export const getSearch = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query('documents')
+      .withIndex('by_user', (query) => query.eq('userId', userId))
+      .filter((query) => query.eq(query.field('isArchived'), false))
+      .order('desc')
+      .collect();
+
+    return documents;
+  }
+});
