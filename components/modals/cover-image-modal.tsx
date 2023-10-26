f8951a960ac3778908152db37fe208f7f5be1a5c
Mohamad Salimi <contact@salimi.my>
2023-10-26 22:43:50 +0800

    Add image cover upload

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -1,10 +1,52 @@
 'use client';
 
+import { useState } from 'react';
+import { useMutation } from 'convex/react';
+import { useParams } from 'next/navigation';
+
+import { api } from '@/convex/_generated/api';
+import { useEdgeStore } from '@/lib/edgestore';
+import { Id } from '@/convex/_generated/dataModel';
 import { useCoverImage } from '@/hooks/use-cover-image';
+import { SingleImageDropzone } from '@/components/single-image-dropzone';
 import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog';
 
 export const CoverImageModal = () => {
+  const params = useParams();
   const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const update = useMutation(api.documents.update);
+
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    coverImage.onClose();
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url
+        }
+      });
+
+      await update({
+        id: params.documentId as Id<'documents'>,
+        coverImage: res.url
+      });
+
+      onClose();
+    }
+  };
 
   return (
     <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
@@ -12,7 +54,12 @@ export const CoverImageModal = () => {
         <DialogHeader>
           <h2 className='text-center text-lg font-semibold'>Cover Image</h2>
         </DialogHeader>
-        <div>Upload Image</div>
+        <SingleImageDropzone
+          className='w-full outline-none'
+          disabled={isSubmitting}
+          value={file}
+          onChange={onChange}
+        />
       </DialogContent>
     </Dialog>
   );
