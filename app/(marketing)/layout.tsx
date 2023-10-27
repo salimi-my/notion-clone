87624af08efb4046aed95afcb323abe54fe68f3b
Mohamad Salimi <contact@salimi.my>
2023-10-28 03:08:57 +0800

    Fix dark bg on nav

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,12 +2,12 @@ import Navbar from '@/components/marketing/navbar';
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className='bg-white dark:bg-[#1F1F1F]'>
       <Navbar />
       <div className='h-full dark:bg-[#1F1F1F]'>
         <main className='h-full pt-40'>{children}</main>
       </div>
-    </>
+    </div>
   );
 };
 
