c566d7647400fb6e8ef8bbc6025f8d40e37f7f73
Mohamad Salimi <contact@salimi.my>
2023-10-28 21:32:27 +0800

    Update text size

--- components/main/item.tsx
+++ components/main/item.tsx
@@ -125,7 +125,7 @@ export const Item = ({
       <span className='truncate'>{label}</span>
       {isSearch && (
         <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
-          <span className='text-xs'>⌘</span>K
+          <span className='text-xs'>⌘</span> <span className='text-sm'>K</span>
         </kbd>
       )}
       {!!id && (
