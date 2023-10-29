e34cd099f25f2d3902608faf5e3b72c8609e7527
Mohamad Salimi <contact@salimi.my>
2023-10-29 16:14:37 +0800

    Update landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,6 +8,7 @@ const MarketingPage = () => {
       <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
         <Heading />
         <Heroes />
+        <hr className='w-full dark:border-zinc-600' />
       </div>
       <Footer />
     </div>
--- components/marketing/heroes.tsx
+++ components/marketing/heroes.tsx
@@ -1,4 +1,7 @@
 import Image from 'next/image';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { ArrowRight } from 'lucide-react';
 
 const Heroes = () => {
   return (
@@ -47,6 +50,36 @@ const Heroes = () => {
           height={481}
           alt='phone-mockup'
           className='absolute bottom-0 -right-14 hidden dark:lg:block'
+        />
+      </div>
+      <div className='flex flex-col justify-center items-center pt-28'>
+        <h2 className='text-6xl font-bold'>Get started for free</h2>
+        <p className='pt-4'>
+          Play around with it first. Get used to it for yourself.
+        </p>
+        <div className='flex justify-center space-x-3 pt-5'>
+          <Button asChild>
+            <Link href='/documents'>Try Motion free</Link>
+          </Button>
+          <Button variant='link' asChild>
+            <Link href='/documents' className='text-sky-500'>
+              Use Motion now <ArrowRight className='h-4 w-4 ml-2' />
+            </Link>
+          </Button>
+        </div>
+        <Image
+          src='/motion-parade-light.png'
+          width={670}
+          height={330}
+          alt='hero'
+          className='pt-7 dark:hidden'
+        />
+        <Image
+          src='/motion-parade-dark.png'
+          width={670}
+          height={330}
+          alt='hero'
+          className='pt-7 hidden dark:block'
         />
       </div>
     </div>
